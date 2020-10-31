d13d646e58144b65e67ef9cf866f50b433714d2e
Aishwarya <amuthyala511@gmail.com>
2020-11-01 01:24:17 +0530

    Checked for duplicate user details in the address book

--- AddressBookJavaStreams/src/com/blz/java/streams/AddressBook.java
+++ AddressBookJavaStreams/src/com/blz/java/streams/AddressBook.java
@@ -7,100 +7,70 @@ import java.util.Scanner;
 public class AddressBook
 {
 	Scanner sc = new Scanner(System.in);
-	public Person p;
-	public String firstName;
-	public AddressBook(String firstName)
+	public static List<Person> personList = new ArrayList<Person>();
+	
+	public void addContact()
 	{
-		this.firstName = firstName;
-	}
-	public Person getPerson()
-	{
-		return p;
-	}
-	public void setPerson(Person p)
-	{
-		this.p = p;
-	}
-	public List<Person> personList = new ArrayList<Person>();
-	public List<Person> getAddressBook()
-	{
-		return personList;
-	}
-	public void setAddressBook(List<Person> personList)
-	{
-		this.personList = personList;
-	}
-	public void addContact(Person p)
-	{
+		System.out.println("Enter first name: ");
+		String firstName = sc.next();
+		for(int i = 0; i < personList.size(); i++)
+		{
+			if(personList.get(i).getFirstName().equals(firstName))
+			{
+				System.out.println("Name already exists. Enter new name");
+				addContact();
+				break;
+			}
+		}
+		System.out.println("Enter last name: ");
+		String lastName = sc.next();
+		System.out.println("Enter address: ");
+		String address = sc.next();
+		System.out.println("Enter city: ");
+		String city = sc.next();
+		System.out.println("Enter state: ");
+		String state = sc.next();
+		System.out.println("Enter zipcode: ");
+		int zip = sc.nextInt();
+		System.out.println("Enter Phone Number: ");
+		long phno = sc.nextLong();
+		System.out.println("Enter email address: ");
+		String emailId = sc.next();
+		Person p = new Person(firstName, lastName, address, city, state, zip, phno, emailId);
 		personList.add(p);
 	}
-	public void editDetails(String firstName)
+	public void deleteDetails()
 	{
-		String str = "";
-		for(Person p : personList)
+		System.out.println("Enter first name: ");
+		String firstName = sc.next();
+		for(int i = 0; i < personList.size(); i++)
 		{
-			str = p.getFirstName();
-			if(firstName.equals(str))
+			if(personList.get(i).getFirstName().equalsIgnoreCase(firstName))
 			{
-				System.out.println("Select the option to edit....\n1. LastName\n2. Address\n3. City\n4. State\5. Zip\n6. PhoneNumber\n7. email\n");
-				int choice = sc.nextInt();
-				switch(choice)
-				{
-				case 1:
-					System.out.println("Enter last name: ");
-					String lastName = sc.next();
-					p.setLastName(lastName); 
-					break;
-				case 2:
-					System.out.println("Enter address: ");
-					String address = sc.next();
-					p.setAddress(address); 
-					break;
-				case 3:
-					System.out.println("Enter city: ");
-					String city = sc.next();
-					p.setCity(city); 
-					break;
-				case 4:
-					System.out.println("Enter state: ");
-					String state = sc.next();
-					p.setState(state); 
-					break;
-				case 5:
-					System.out.println("Enter zipcode: ");
-					int zip = sc.nextInt();
-					p.setZip(zip); 
-					break;
-				case 6:
-					System.out.println("Enter Phone Number: ");
-					long phno = sc.nextLong();
-					p.setPhNo(phno); 
-					break;
-				case 7:
-					System.out.println("Enter email address: ");
-					String emailId = sc.next();
-					p.setEmailId(emailId); 
-					break;
-				default:
-					System.out.println("Invalid choice");
-				}
+				personList.remove(i);
 			}
 			else
 			{
-				System.out.println("No matches found. Enter a valid first name");
+				System.out.println("No matches found");
 			}
 		}
 	}
-	public void deleteDetails(String firstName)
+	public void addPerson()
 	{
-		String str1 = "";
-		for(Person p : personList)
+		System.out.println("Enter number of persons to be added to the address book: ");
+		int noOfPersons = sc.nextInt();
+		int count = 1;
+		while(count <= noOfPersons)
 		{
-			str1 = p.getFirstName();
-			if(firstName.equals(str1))
-			{
-				personList.remove(p);
-			}
+			addContact();
+			count++;
 		}
+	}
+	public static void main(String[] args)
+	{
+		AddressBook adrBook = new AddressBook();
+		System.out.println("Welcome to the Address Book Problem");
+		adrBook.addContact();
+		System.out.println(personList);
 	}
 }
